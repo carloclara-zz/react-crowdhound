b85aaf09727c5685a399d6964e42352ba1f4c95a
Carlo Clara <carlo.clara@tooltwist.com>
2020-07-09 01:45:52 +0800

    Dynamic likes and comments updates

--- src/components/NewsFeed/Comments/CommentSingle.js
+++ src/components/NewsFeed/Comments/CommentSingle.js
@@ -1,10 +1,15 @@
-import React, { useState, crowdhound } from 'react'
+import React, { useEffect, useState, crowdhound } from 'react'
+import moment from 'moment'
 import ShowMore from 'react-show-more'
 import Comments from './Comments'
 import anonymousAvatar from '../../../assets/images/anonymous-avatar.png'
 
 const CommentSingle = ({ element = {} }) => {
+  const [data, setData] = useState(element)
   const [comment, setComment] = useState('')
+  useEffect(() => {
+    setData(element)
+  }, [element])
   const insertComment = async (e, parentId) => {
     if (e.keyCode === 13) {
       const payload = {
@@ -16,11 +21,22 @@ const CommentSingle = ({ element = {} }) => {
         status: 'active',
         deleted: 'false'
       }
-      const reply = await crowdhound.create(this, payload)
       console.log('comment payload:', payload)
+      const createReponse = await crowdhound.create(this, payload)
+      console.log('createReponse:', createReponse)
+
+      const selectResponse = await crowdhound.select(this, {
+        elementId: parentId,
+        withChildren: true
+      })
+      console.log('selectResponse:', selectResponse)
+      if (selectResponse.elements[0]) {
+        setData(selectResponse.elements[0])
+      }
       setComment('')
     }
   }
+
   const likeComment = async ({
     id,
     rootId,
@@ -30,32 +46,48 @@ const CommentSingle = ({ element = {} }) => {
   }) => {
     const parsedExtraProperties = JSON.parse(extraProperties) || {}
     const likes = parsedExtraProperties.likes || []
-    likes.push({ userId: 'johnjoe', name: 'John Joe' })
+    // Prevent duplicate user inserts
+    const filteredLikes = likes.filter((obj) => {
+      return obj.userId !== 'johnjoe'
+    })
+    // Insert new object if is not existing in the 'likes' array
+    if (likes.length === filteredLikes.length) {
+      filteredLikes.push({ userId: 'johnjoe', name: 'John Joe' })
+    }
     const payload = {
       id,
       rootId,
       parentId,
       description,
-      extraProperties: JSON.stringify({ likes })
+      extraProperties: JSON.stringify({ likes: filteredLikes })
     }
-    const reply = await crowdhound.update(this, payload)
     console.log('like payload: ', payload)
+    await crowdhound.update(this, payload)
+
+    const selectResponse = await crowdhound.select(this, {
+      elementId: id,
+      withChildren: true
+    })
+    console.log('selectResponse:', selectResponse)
+    if (selectResponse.elements[0]) {
+      setData(selectResponse.elements[0])
+    }
   }
+
   const notificationCounter = (element) => {
     const extraProperties = JSON.parse(element.extraProperties) || {}
     let totalLikes = 0
-    let activeLike = ''
-    const namesToopTip = []
+    let filteredUserLike = []
+    const namesToolTip = []
     if (extraProperties.likes) {
       totalLikes = extraProperties.likes.length
-      const filteredUserLike = extraProperties.likes.filter((obj) => {
+      // Check if the current user already liked the comment
+      filteredUserLike = extraProperties.likes.filter((obj) => {
         return obj.userId === 'johnjoe'
       })
-      if (filteredUserLike.length > 0) {
-        activeLike = 'liked'
-      }
+      // Collect all names who've liked the comment
       extraProperties.likes.forEach((obj) => {
-        namesToopTip.push(obj.name)
+        namesToolTip.push(obj.name)
       })
     }
     const totalComments = element.children.length
@@ -63,7 +95,7 @@ const CommentSingle = ({ element = {} }) => {
       <div>
         <div className='ch-notification-wrapper'>
           {totalLikes > 0 && (
-            <span title={namesToopTip.join()}>
+            <span title={namesToolTip.join()}>
               {totalLikes} {totalLikes > 1 ? 'Likes' : 'Like'}
             </span>
           )}
@@ -73,8 +105,11 @@ const CommentSingle = ({ element = {} }) => {
             </span>
           )}
         </div>
-        <div className='ch-action-cell'>
-          <label onClick={() => likeComment(element)} className={activeLike}>
+        <div className='ch-action-wrapper'>
+          <label
+            onClick={() => likeComment(element)}
+            className={filteredUserLike.length > 0 ? 'liked' : ''}
+          >
             Like
           </label>
           <label
@@ -88,6 +123,11 @@ const CommentSingle = ({ element = {} }) => {
       </div>
     )
   }
+
+  const dateFromNow = (date) => {
+    return moment(date * 1000).fromNow()
+  }
+
   return (
     <div>
       <table className='ch-comments-maintable'>
@@ -98,7 +138,10 @@ const CommentSingle = ({ element = {} }) => {
             </td>
             <td className='ch-comments-right-cell'>
               <span className='ch-comments-userid'>
-                {element.userId === '0' ? 'Unknown' : element.userId}
+                {data.userId === '0' ? 'Unknown' : data.userId}
+              </span>
+              <span className='ch-comments-date'>
+                {dateFromNow(data.created)}
               </span>
               <div className='ch-comments-description'>
                 <ShowMore
@@ -107,12 +150,12 @@ const CommentSingle = ({ element = {} }) => {
                   less='See Less'
                   anchorClass='ch-see-more-content'
                 >
-                  <p>{element.description}</p>
+                  <p>{data.description}</p>
                 </ShowMore>
               </div>
-              {notificationCounter(element)}
-              <Comments elements={element.children} />
-              {element.type === 'newsFeedComment' && (
+              {notificationCounter(data)}
+              <Comments elements={data.children} />
+              {data.type === 'newsFeedComment' && (
                 <div>
                   <table className='ch-comments-maintable'>
                     <tbody>
@@ -125,12 +168,12 @@ const CommentSingle = ({ element = {} }) => {
                         </td>
                         <td className='ch-comments-right-cell'>
                           <input
-                            id={element.id}
+                            id={data.id}
                             className='ch-comments-textbox'
                             type='text'
                             placeholder='Write a comment...'
                             value={comment}
-                            onKeyUp={(e) => insertComment(e, element.id)}
+                            onKeyUp={(e) => insertComment(e, data.id)}
                             onChange={(e) => {
                               setComment(e.target.value)
                             }}
--- src/components/NewsFeed/NewsFeedSingle.js
+++ src/components/NewsFeed/NewsFeedSingle.js
@@ -5,7 +5,11 @@ import Comments from './Comments'
 import anonymousAvatar from '../../assets/images/anonymous-avatar.png'
 
 const NewsFeedSingle = ({ element = {} }) => {
+  const [data, setData] = useState(element)
   const [comment, setComment] = useState('')
+  useEffect(() => {
+    setData(element)
+  }, [element])
   const insertComment = async (e, parentId) => {
     if (e.keyCode === 13) {
       const payload = {
@@ -17,15 +21,107 @@ const NewsFeedSingle = ({ element = {} }) => {
         status: 'active',
         deleted: 'false'
       }
-      const reply = await crowdhound.create(this, payload)
       console.log('comment:', payload)
+      const createReponse = await crowdhound.create(this, payload)
+      console.log('createReponse:', createReponse)
+
+      const selectResponse = await crowdhound.select(this, {
+        elementId: parentId,
+        withChildren: true
+      })
+      console.log('selectResponse:', selectResponse)
+      if (selectResponse.elements[0]) {
+        setData(selectResponse.elements[0])
+      }
       setComment('')
     }
+  }
+
+  const likeComment = async ({
+    id,
+    rootId,
+    parentId,
+    description,
+    extraProperties
+  }) => {
+    const parsedExtraProperties = JSON.parse(extraProperties) || {}
+    const likes = parsedExtraProperties.likes || []
+    // Prevent duplicate user inserts
+    const filteredLikes = likes.filter((obj) => {
+      return obj.userId !== 'johnjoe'
+    })
+    // Insert new object if is not existing in the 'likes' array
+    if (likes.length === filteredLikes.length) {
+      filteredLikes.push({ userId: 'johnjoe', name: 'John Joe' })
+    }
+    const payload = {
+      id,
+      rootId,
+      parentId,
+      description,
+      extraProperties: JSON.stringify({ likes: filteredLikes })
+    }
+    console.log('like payload: ', payload)
+    await crowdhound.update(this, payload)
+
+    const selectResponse = await crowdhound.select(this, {
+      elementId: id,
+      withChildren: true
+    })
+    console.log('selectResponse:', selectResponse)
+    if (selectResponse.elements[0]) {
+      setData(selectResponse.elements[0])
+    }
+  }
+
+  const notificationCounter = (element) => {
+    const extraProperties = JSON.parse(element.extraProperties) || {}
+    let totalLikes = 0
+    let filteredUserLike = []
+    const namesToolTip = []
+    if (extraProperties.likes) {
+      totalLikes = extraProperties.likes.length
+      // Check if the current user already liked the comment
+      filteredUserLike = extraProperties.likes.filter((obj) => {
+        return obj.userId === 'johnjoe'
+      })
+      // Collect all names who've liked the comment
+      extraProperties.likes.forEach((obj) => {
+        namesToolTip.push(obj.name)
+      })
+    }
+    const totalComments = element.children.length
+    return (
+      <div>
+        <div className='ch-notification-wrapper'>
+          {totalLikes > 0 && (
+            <span title={namesToolTip.join()}>
+              {totalLikes} {totalLikes > 1 ? 'Likes' : 'Like'}
+            </span>
+          )}
+          {totalComments > 0 && (
+            <span>
+              {totalComments} {totalComments > 1 ? 'Comments' : 'Comment'}
+            </span>
+          )}
+        </div>
+        <div className='ch-action-wrapper'>
+          <label
+            onClick={() => likeComment(element)}
+            className={filteredUserLike.length > 0 ? 'liked' : ''}
+          >
+            Like
+          </label>
+          <label htmlFor={element.id}>Comment</label>
+        </div>
+      </div>
+    )
   }
 
   const dateFromNow = (date) => {
     return moment(date * 1000).fromNow()
   }
+
   return (
     <div className='ch-newsfeed-wrapper'>
       <table className='ch-newsfeed-maintable'>
@@ -36,63 +132,45 @@ const NewsFeedSingle = ({ element = {} }) => {
             </td>
             <td className='ch-newsfeed-right-cell'>
               <span className='ch-newsfeed-userid'>
-                {element.userId === '0' ? 'Unknown' : element.userId}
+                {data.userId === '0' ? 'Unknown' : data.userId}
               </span>
               <span className='ch-newsfeed-date'>
-                {dateFromNow(element.created)}
+                {dateFromNow(data.created)}
               </span>
             </td>
           </tr>
+        </tbody>
+      </table>
+      <div className='ch-newsfeed-description'>
+        <ShowMore
+          lines={3}
+          more='See More'
+          less='See Less'
+          anchorClass='ch-see-more-content'
+        >
+          <p>{data.description}</p>
+        </ShowMore>
+      </div>
+      {notificationCounter(data)}
+      <Comments elements={data.children} />
+      <table className='ch-comments-maintable'>
+        <tbody>
           <tr>
-            <td colSpan='2' className='ch-newsfeed-description'>
-              <ShowMore
-                lines={3}
-                more='See More'
-                less='See Less'
-                anchorClass='ch-see-more-content'
-              >
-                <p>{element.description}</p>
-              </ShowMore>
+            <td className='ch-comments-left-cell'>
+              <img className='ch-comments-avatar' src={anonymousAvatar} />
             </td>
-          </tr>
-          <tr>
-            <td colSpan='2' className='ch-action-cell'>
-              <label>Like</label>
-              <label htmlFor={element.id}>Comment</label>
-            </td>
-          </tr>
-          <tr>
-            <td colSpan='2'>
-              <Comments elements={element.children} />
-            </td>
-          </tr>
-          <tr>
-            <td colSpan='2'>
-              <table className='ch-comments-maintable'>
-                <tbody>
-                  <tr>
-                    <td className='ch-comments-left-cell'>
-                      <img
-                        className='ch-comments-avatar'
-                        src={anonymousAvatar}
-                      />
-                    </td>
-                    <td className='ch-comments-right-cell'>
-                      <input
-                        id={element.id}
-                        className='ch-comments-textbox'
-                        type='text'
-                        placeholder='Write a comment...'
-                        value={comment}
-                        onKeyUp={(e) => insertComment(e, element.id)}
-                        onChange={(e) => {
-                          setComment(e.target.value)
-                        }}
-                      />
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
+            <td className='ch-comments-right-cell'>
+              <input
+                id={data.id}
+                className='ch-comments-textbox'
+                type='text'
+                placeholder='Write a comment...'
+                value={comment}
+                onKeyUp={(e) => insertComment(e, data.id)}
+                onChange={(e) => {
+                  setComment(e.target.value)
+                }}
+              />
             </td>
           </tr>
         </tbody>
